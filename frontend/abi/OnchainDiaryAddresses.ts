2350b78822e00883ae2aec9006f51b160a3b43bd
vopatas <vopatas@proton.me>
2025-10-27 20:37:17 +0100

    Add OnchainDiaryAddresses.ts

---
+++ frontend/abi/OnchainDiaryAddresses.ts
@@ -0,0 +1,7 @@
+export const OnchainDiaryAddresses = {
+  "11155111": {
+    "address": "0xF020eBc04385e6E6AB311d19cCa1b0988134384a",
+    "chainId": 11155111,
+    "chainName": "sepolia"
+  }
+} as const;
